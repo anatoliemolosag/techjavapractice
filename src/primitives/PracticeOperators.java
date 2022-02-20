845e09c225e4a72e0f32aef31b42e152615a0725
anatoliemolosag <anatolie.molosag94@gmail.com>
2022-02-19 21:25:00 -0600

    new

---
+++ src/primitives/PracticeOperators.java
@@ -0,0 +1,14 @@
+package primitives;
+
+public class PracticeOperators {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int i;
+        i= a++ + ++a + --a;
+        System.out.println(i);
+
+
+
+    }
+}
