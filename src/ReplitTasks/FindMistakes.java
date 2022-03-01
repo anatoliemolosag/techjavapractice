57a9883a36ea9f1064bd6ead085eef4f445a5b18
anatoliemolosag <anatolie.molosag94@gmail.com>
2022-03-01 15:35:38 -0600

    uppp

---
+++ src/ReplitTasks/FindMistakes.java
@@ -0,0 +1,45 @@
+package ReplitTasks;
+
+public class FindMistakes {
+    public static void main(String[] args) {
+
+    // Do not change the texts or variable values
+
+    System.out.println("Hello world!");
+
+    System.out.println("hello world");
+
+    //Hint for below look for right data type
+    double number = 43.32;
+
+    System.out.println(number);
+
+    float float1= 43.1f;
+
+    System.out.println(float1);
+
+    System.out.println("I love java");
+
+    boolean bl = true;
+
+    System.out.println(bl);
+
+    long l = 4567877777776l;
+
+    System.out.println(l);
+
+
+    /*You have given code with a lot of errors in main class.
+     You should find all errors in fix the code.
+     Please do not add any other print line or anything just fix the compile errors and run the code.
+     When you are able to compile and run the code, tests will pass.
+
+     Good Luck!
+     Techtorial
+
+     */
+
+
+}
+}
+
