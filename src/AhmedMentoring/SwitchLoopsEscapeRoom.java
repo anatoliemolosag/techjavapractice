2d0853c0b9eba0de12901d005458aefbce304981
anatoliemolosag <anatolie.molosag94@gmail.com>
2022-03-10 23:11:49 -0600

    update

--- src/AhmedMentoring/SwitchLoopsEscapeRoom.java
+++ src/AhmedMentoring/SwitchLoopsEscapeRoom.java
@@ -1,2 +1,18 @@
-package AhmedMentoring;public class SwitchLoopsEscapeRoom {
-}
+package AhmedMentoring;
+
+public class SwitchLoopsEscapeRoom {
+    public static void main(String[] args) {
+
+            int variable =7;
+            switch (variable++)
+            {
+                case 7:
+                    switch (variable--) {
+                        default:
+                            System.out.print(variable);
+                        case 7:
+                    }
+                        default:
+                    System.out.print ("wonders");
+
+}}}
