ab2c92e10839d710256ff006cc3ad7cbeaeecfc1
anatoliemolosag <anatolie.molosag94@gmail.com>
2022-03-11 10:16:04 -0600

    update1

---
+++ src/Loops/For/NestedForLoops.java
@@ -0,0 +1,13 @@
+package Loops.For;
+
+public class NestedForLoops {
+    public static void main(String[] args) {
+
+        for(int i =0;i<5;i++){
+            for (int k =0;k<3;k++){
+                System.out.println("i>> " +i);
+                System.out.println("k** " +k);
+            }
+        }
+    }
+}
