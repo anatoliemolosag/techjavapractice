e298638bddaa1fdb27a1017250aae1ffbf510964
anatoliemolosag <anatolie.molosag94@gmail.com>
2022-04-26 19:07:25 -0500

    update2

---
+++ src/pack/main.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class main {
+    public static void main(String[] args) {
+        mouse1 obj = new mouse1();
+        mouse obj1 = new mouse();
+        obj1.ScrollUp();
+
+    }
+}
