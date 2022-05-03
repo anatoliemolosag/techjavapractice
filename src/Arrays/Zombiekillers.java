83e14359bc484de76ab3a1d26d72db0da371b9a1
anatoliemolosag <anatolie.molosag94@gmail.com>
2022-05-03 10:27:54 -0500

    update5

---
+++ src/Arrays/Zombiekillers.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Zombiekillers {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our zombie killer program");
+        String backpack[] = {"Shotgun", "Assault Rifle", "Sniper"};
+        String zombies[] ={"Close-Range Zombie" , "Mid-range Zombie" , "Long-Range Zombie"};
+
+        System.out.println("Backpack Items");
+        System.out.println(backpack[0]);
+        System.out.println(backpack[1]);
+        System.out.println(backpack[2]);
+
+        System.out.println("These are the Zombies");
+        System.out.println(zombies[0]);
+        System.out.println(zombies[1]);
+        System.out.println(zombies[2]);
+
+        int numbersZombieHate[] ={4,90,70,123,4,561,1};
+        System.out.println(numbersZombieHate[6]);
+
+
+    }
+}
