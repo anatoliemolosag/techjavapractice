ae9234cda120cd5fafe5ec6635dfa2dc575843f5
anatoliemolosag <anatolie.molosag94@gmail.com>
2022-05-03 10:29:53 -0500

    update5

---
+++ src/AhmedMentoring/DebuggingAndReplaceJava/ListOfConcept.java
@@ -0,0 +1,32 @@
+package AhmedMentoring.DebuggingAndReplaceJava;
+
+public class ListOfConcept {
+    public static void main(String[] args) {
+
+        /*
+1-Java / Primitive Data / ASCII table  ------>Done
+2- String  / String methods  ----> **AUTOMATION** --> DONE
+3- Loops -->DONE
+4- If Statement --> DONE
+5- Ternary Operators --> DONE
+6- Switch Statement -->DONE
+7- Arrays --> DONE -->DONE
+8- String split Method -->DONE
+9- Object - Methods -->DONE
+10-Constructor -->DONE
+11-Garbage Collection -->DONE
+12-Wrapper Classes --> DONE
+13-MultiDimensionalArray -->Done
+13-ArrayList -->  Automation a Lot  --->DONE
+14-Set --> DONE
+15-Map --> DONE
+16-Static-Instance Block --> DONE
+17-StringBuilder --> DONE
+18-Encapsulation --> DONE
+19-Inheritance -- overloading - overriding
+20-Abstract-Interface
+21-Polymorfism
+22-Exception
+ */
+    }
+}
